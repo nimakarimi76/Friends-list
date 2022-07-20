2f729cd342288159df27f27049c3c4d21ee39ac9
nimakarimi76 <nimakarimi676@gmail.com>
2022-07-20 15:32:53 +0200

    Added about us and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'home/about'
 end
