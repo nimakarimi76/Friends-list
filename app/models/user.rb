1ce41cef3c38f2e4e9e889b0fb55aa333f6ffb22
nimakarimi76 <nimakarimi676@gmail.com>
2022-07-24 11:55:24 +0200

    Each user has their own friends (belong_to)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  # has_many :friends, class_name: "friend", foreign_key: "reference_id"
+  has_many :friends
 end
