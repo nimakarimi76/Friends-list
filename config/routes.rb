d09122c053f50935bb4400c82e317a4fb3371827
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-14 17:35:23 +0200

    updated packages

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # end
 
   resources :friends
-  root 'friends#index'
+  root 'home#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
