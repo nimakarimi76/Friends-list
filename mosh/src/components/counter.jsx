f3fd1ef3a2eba86394f74ea260b291870754d9e5
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-15 18:18:12 +0200

    jsx basics

--- mosh/src/components/counter.jsx
+++ mosh/src/components/counter.jsx
@@ -1,24 +1,57 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // const [count, setCount] = useState(0)
   state = {
-    count: 0,
+    count: 3,
+    tags: ["tag 1", "tag 2", "tag 3"],
   };
+
   handleIncrement = () => {
-    this.state.count++;
-    console.log(this.state.count);
+    console.log(this.state);
   };
+
+  style = {
+    color: "green",
+    padding: "1rem",
+    textAlign: "center",
+  };
+
+  renderTags() {
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}> {tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div className="container">
-        <h1 className="card m-3"> Counter App</h1>
+        <h1 style={this.style} className="card m-3">
+          Counter App
+        </h1>
         <button className="btn btn-primary m-3" onClick={this.handleIncrement}>
           increment
         </button>
-        <span> {this.state.count}</span>
+        <h4 style={{ padding: "1rem" }} className={this.getCountClasses()}>
+          {this.formatCount()}
+        </h4>
+        {this.state.tags.length == 0 && "Please create a tag"}
+        {this.renderTags()}
       </div>
     );
+  }
+  formatCount() {
+    let { count } = this.state;
+    return count === 0 ? <h3 className="text-danger">Zero</h3> : count;
+  }
+
+  getCountClasses() {
+    let classes = "m-2 text-";
+    classes += this.state.count % 2 === 0 ? "info" : "success";
+    return classes;
   }
 }
 
