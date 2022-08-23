359d941a0c30af91a562f6daba62dc9c18e4a7ef
nimakarimi76 <nimakarimi676@gmail.com>
2022-08-23 19:52:34 +0200

    fetch with useEffect

--- mosh/src/index.js
+++ mosh/src/index.js
@@ -7,6 +7,7 @@ import Movies from "./components/movies";
 import App from "./App";
 import Excuse from "./components/excuses";
 import MoviesDetails from "./components/moviesDetails";
+import Users from "./components/users";
 
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
@@ -14,11 +15,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <div className="">
-      <Link to="/"> Home </Link>
+      <Link to="/">Home</Link>
       <Link className="m-5" to="/movies">
-        Movies{" "}
+        Movies
       </Link>
-      <Link to="/Excuse"> Excuser </Link>
+      <Link to="/Excuse">Excuser</Link>
+      <Link className="mx-4" to="/Users">
+        Users
+      </Link>
     </div>
     <Routes>
       <Route path="/movies/:id" element={<MoviesDetails />} />
@@ -27,9 +31,9 @@ root.render(
         render={(props) => <Movies {...props} />}
         element={<Movies />}
       />
-
       {/* <Route path="/movies" element={<Movies />} /> */}
       <Route path="/excuse" element={<Excuse />} />
+      <Route path="/users" element={<Users />} />
       <Route path="*" element={<h1>Page not found</h1>} />
       <Route path="/" element={<App />} />
     </Routes>
